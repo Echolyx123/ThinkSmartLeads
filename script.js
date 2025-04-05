e788f2ed5ad1b730542692b457eab376120b3be5
Echolyx123 <nemecekmilan9@gmail.com>
2025-04-05 12:04:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
       };
 
       try {
-        const response = await fetch("https://thinksmart-api.onrender.com/api/submit", {
+        const response = await fetch("https://thinksmart-api.onrender.com", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(data)
